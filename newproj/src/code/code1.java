be90b330ee48a1e0a12765e08b930f213fb2da2d
adsairam <adsairam@DIN73003280.corp.capgemini.com>
2020-07-01 18:02:34 +0530

    2nd commit

--- newproj/src/code/code1.java
+++ newproj/src/code/code1.java
@@ -4,8 +4,13 @@ public class code1 {
 
 	public static void main(String[] args) {
 		
+		int a,b;
 		
-		System.out.println("hi!! Lets push this to GIT");
+		a=5;b=10;
+		
+		int c= a+b;
+		
+		System.out.println("hi !! Lets push this to GIT "+c);
 
 	}
 
